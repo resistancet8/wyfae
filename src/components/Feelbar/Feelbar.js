7d950f461800a3a7f28161cc1a0cf25d51841beb
naveen <resistancet9@gmail.com>
2018-09-26 15:33:09 +0530

    added icons

--- src/components/Feelbar/Feelbar.js
+++ src/components/Feelbar/Feelbar.js
@@ -2,26 +2,31 @@ import React, { Component } from "react";
 import { Route, Switch, NavLink } from "react-router-dom";
 import { Button } from "reactstrap";
 import "./Feelbar.css";
+import FeelCircle from "./../../assets/img/feelcircle.svg";
+import JournalCircle from "./../../assets/img/journal icon.svg";
+import FeelRequest from "./../../assets/img/feelRequest.svg";
+import IconButton from "@material-ui/core/IconButton";
 
+console.log(FeelCircle);
 class Feelbar extends Component {
   render() {
     return (
       <div className="Feelbar d-flex mb-3">
         <div className="ml-auto">
           <NavLink className="p-2 text-dark" to="/friends">
-            <Button outline color="primary">
-              Friend Circle
-            </Button>
+            <IconButton color="" className="icon-holder">
+              <img src={FeelCircle} alt="" />
+            </IconButton>
           </NavLink>
           <NavLink className="p-2 text-dark" to="/requests">
-            <Button outline color="info">
-              Requests
-            </Button>
+            <IconButton color="" className="icon-holder">
+              <img src={FeelRequest} alt="" />
+            </IconButton>
           </NavLink>
           <NavLink className="p-2 text-dark" to="/journal">
-            <Button outline color="success">
-              Journal
-            </Button>
+            <IconButton color="" className="icon-holder">
+              <img src={JournalCircle} alt="" />
+            </IconButton>
           </NavLink>
         </div>
       </div>
