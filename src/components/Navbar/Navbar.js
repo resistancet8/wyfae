5b3b633ccf98d9b7d7ba47e7c142e1729d554405
Naveen <resistancet8@gmail.com>
2019-08-17 19:28:18 +0530

    notifications

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { NavLink, withRouter } from "react-router-dom";
+import { NavLink, withRouter, Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { logoutUser } from "./../../actions/auth_actions";
 import Brand from "./../../assets/img/wyfae_main logo.svg";
@@ -9,11 +9,14 @@ import IconButton from "@material-ui/core/IconButton";
 import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
-import AccessAlarmIcon from "@material-ui/icons/AccountCircle";
 import FeelCircle from "./../../assets/img/feelcircle.svg";
 import JournalCircle from "./../../assets/img/journal icon.svg";
 import SearchItem from "./SearchItem";
 import { Scrollbars } from "react-custom-scrollbars";
+import Loader from './../Loader';
+import InfiniteScroll from 'react-infinite-scroller';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import moment from 'moment';
 
 import "./Navbar.css";
 import Axios from "axios";
@@ -21,8 +24,31 @@ import Axios from "axios";
 class NavbarComponent extends Component {
   state = {
     anchorEl: null,
-    results: []
+    results: [],
+    notifications: [],
+    hasMore: true,
+    showLoading: false,
+    showNotification: false
   };
+
+  componentDidMount() {
+    this.getNotifications();
+    window.addEventListener("click", e => {
+      e.preventDefault();
+      this.setState({
+        showNotification: false
+      })
+    });
+
+    window.addEventListener("scroll", e => {
+      e.preventDefault();
+      if (this.state.showNotification) {
+        this.setState({
+          showNotification: false
+        })
+      }
+    });
+  }
 
   handleClick = event => {
     this.setState({ anchorEl: event.currentTarget });
@@ -47,16 +73,57 @@ class NavbarComponent extends Component {
     })
       .then(d => {
         this.setState({
-          results: d.data.content
+          results: d.data.all_content
         });
       })
-      .catch(e => {});
+      .catch(e => { });
   }
 
   handleShowResult() {
     if (document.querySelector(".search-results")) {
       document.querySelector(".search-results").style.display = "block";
     }
+  }
+
+  loadFunc() {
+    Axios
+      .post(`${process.env.REACT_APP_API_ENDPOINT}/user/get_notification`, { skip_count: this.state.notifications.length, limit_count: 10 })
+      .then(response => {
+        this.setState((prev) => {
+          return {
+            notifications: [...this.state.notifications, ...response.data.all_content],
+            hasMore: response.data.all_content.length <= 0 ? false : true
+          }
+        })
+      })
+      .catch(err => {
+      });
+  }
+
+  handleShowNotification(e) {
+    e.stopPropagation();
+    this.setState({
+      showNotification: !this.state.showNotification
+    })
+  }
+
+  getNotifications() {
+
+    this.setState({
+      showLoading: true
+    });
+
+    Axios.post(`${process.env.REACT_APP_API_ENDPOINT}` + "/user/get_notification", {
+      "skip_count": 0,
+      "limit_count": 10
+    })
+      .then(d => {
+        this.setState({
+          notifications: [...d.data.all_content],
+          showLoading: d.data.all_content.length == 0 ? false : true
+        });
+      })
+      .catch(e => { });
   }
 
   render() {
@@ -76,12 +143,42 @@ class NavbarComponent extends Component {
           );
         })
       ) : (
-        <SearchItem empty={1} />
-      );
+          <SearchItem empty={1} />
+        );
+
+    let notificationsHTML = this.state.notifications.map(notification => {
+      let notificationHTML = [];
+
+      switch (notification.type) {
+        case 'liked':
+          notificationHTML.push(<Link onClick={e => { this.setState({ showNotification: false }) }} to={`/view/${notification.post_id}`}><ListGroupItem className="each-notification p-3"><i class="fas fa-thumbs-up"></i>&nbsp;&nbsp;&nbsp;<Link to={`/profile/${notification.username}`}><strong className="font-weight-bold">{notification.name}</strong></Link> liked <strong className="font-weight-bold">{notification.post_title}</strong>. <div class="pl-2 mt-2"><strong>{moment(notification.creation_time).local().fromNow()}</strong></div></ListGroupItem></Link>);
+          break;
+        case 'unliked':
+          notificationHTML.push(<Link onClick={e => { this.setState({ showNotification: false }) }} to={`/view/${notification.post_id}`}><ListGroupItem className="each-notification p-3"><i class="fas fa-thumbs-down"></i>&nbsp;&nbsp;&nbsp;<Link to={`/profile/${notification.username}`}><strong className="font-weight-bold">{notification.name}</strong></Link> unliked <strong className="font-weight-bold">{notification.post_title}</strong>. <div class="pl-2 mt-2"><strong>{moment(notification.creation_time).local().fromNow()}</strong></div></ListGroupItem></Link>);
+          break;
+
+        case 'commented':
+          notificationHTML.push(<Link onClick={e => { this.setState({ showNotification: false }) }} to={`/view/${notification.post_id}`}><ListGroupItem className="each-notification p-3"><i class="fas fa-comments"></i>&nbsp;&nbsp;&nbsp;<Link to={`/profile/${notification.username}`}><strong className="font-weight-bold">{notification.name}</strong></Link> commented <strong className="font-weight-bold">{notification.comment_text}</strong> on <strong className="font-weight-bold">{notification.post_title}</strong>. <div class="pl-2 mt-2"><strong>{moment(notification.creation_time).local().fromNow()}</strong></div></ListGroupItem></Link>);
+          break;
+
+        case 'followed':
+          notificationHTML.push(<Link onClick={e => { this.setState({ showNotification: false }) }} to={`/profile/${notification.followed_by}`}><ListGroupItem className="each-notification p-3"><i class="fas fa-user-plus"></i>&nbsp;&nbsp;&nbsp;<Link to={`/profile/${notification.followed_by}`}><strong className="font-weight-bold">{notification.followed_by}</strong></Link> started following you. <div class="pl-2 mt-2"><strong>{moment(notification.creation_time).local().fromNow()}</strong></div></ListGroupItem></Link>);
+          break;
+
+        case 'unfollowed':
+          notificationHTML.push(<Link onClick={e => { this.setState({ showNotification: false }) }} to={`/profile/${notification.followed_by}`}><ListGroupItem className="each-notification p-3"><i class="fas fa-user-minus"></i>&nbsp;&nbsp;&nbsp;<Link to={`/profile/${notification.followed_by}`}><strong className="font-weight-bold">{notification.followed_by}</strong></Link> unfollowed   you. <div class="pl-2 mt-2"><strong>{moment(notification.creation_time).local().fromNow()}</strong></div>  </ListGroupItem></Link>);
+          break;
+
+        default:
+          break;
+      }
+
+      return notificationHTML;
+    });
 
     return (
       <header>
-        <div className={`d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm navbar-holder-design justify-content-between ${window.location.pathname.indexOf('login') >= 0 || window.location.pathname.indexOf('register') >= 0 ? "": "add-gradient"} `}>
+        <div className={`d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white border-bottom shadow-sm navbar-holder-design justify-content-between ${window.location.pathname.indexOf('login') >= 0 || window.location.pathname.indexOf('register') >= 0 ? "" : "add-gradient"} `}>
           <NavLink className="my-0 banner" to="/trending">
             <img src={Brand} alt="Wyfae Brand" />
           </NavLink>
@@ -114,7 +211,35 @@ class NavbarComponent extends Component {
               </div>
             </div>
           )}
-          <nav className="my-2 my-md-0 mr-md-3 nav-links-h">
+          <nav style={{ position: 'relative' }} className="my-2 my-md-0 mr-md-3 nav-links-h">
+            <div style={{ position: 'absolute' }} className={`${this.state.showNotification ? 'd-grid' : 'd-none'} notifications-holder`}>
+              <div className="notifications-header pl-3">
+                <span>Notifications</span>
+              </div>
+              <div className="notifications shadow bg-light">
+                {this.state.notifications && this.state.notifications.length > 0 ?
+                  <div style={{ maxHeight: "600px", overflowX: "hidden", overflowY: "auto", position: 'relative' }}>
+                    <Scrollbars autoHeight autoHide autoHeightMax={600}>
+                      <InfiniteScroll
+                        loadMore={this.loadFunc.bind(this)}
+                        hasMore={this.state.hasMore}
+                        useWindow={false}
+                        threshold={10}
+                        loader={<div className="text-center py-2"> <Loader /></div>}
+                      >
+                        <ListGroup className="notifications-parent">
+                          {notificationsHTML}
+                        </ListGroup>
+                      </InfiniteScroll>
+                    </Scrollbars>
+                  </div> :
+                  <div>
+                    {this.state.showLoading && <div className="text-center py-2"> <Loader /></div>}
+                    {this.state.notifications.length == 0 && !this.state.showLoading && <div className="text-center py-2"><strong>No notifications to show.</strong></div>}
+                  </div>
+                }
+              </div>
+            </div>
             {!isAuth && (
               <React.Fragment>
                 <NavLink className="p-2 text-dark" to="/register">
@@ -142,7 +267,12 @@ class NavbarComponent extends Component {
                     <img src={TrendingFeel} alt="" />
                   </IconButton>
                 </NavLink>
-                <Button onClick={this.handleClick} style={{color: "white"}}>
+                <span className="p-2 text-dark" onClick={this.handleShowNotification.bind(this)}>
+                  <IconButton color="default" className="icon-holder2">
+                    <i className="fas fa-bell"></i>
+                  </IconButton>
+                </span>
+                <Button onClick={this.handleClick} style={{ color: "white" }}>
                   <img
                     src={`${process.env.REACT_APP_API_ENDPOINT}` + "/" + profile_img}
                     className="img-fluid"
