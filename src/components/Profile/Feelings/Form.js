599cf26bd415c0eee42826cd5bc1ff5dc0acd266
Naveen <resistancet8@gmail.com>
2020-01-13 10:05:47 +0530

    fix

--- src/components/Profile/Feelings/Form.js
+++ src/components/Profile/Feelings/Form.js
@@ -555,6 +555,11 @@ class Form extends Component {
 																		/>
 																		<Carousel
 																			selectedItem={this.state.selectedItem}
+																			showIndicators={false}
+																			swipeable={true}
+																			infiniteLoop={true}
+																			dynamicHeight={true}
+																			thumbWidth={500}
 																			onClickItem={(e) => {
 																				this.selectImage(
 																					`${process.env
